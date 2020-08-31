42d292fb686f6ccd1df0ef7a68ce5fc0b78887e8
Alec Trievel <atrievel@ceiamerica.com>
2020-08-31 14:40:32 -0400

    Add schema and environment vars for Postgres

--- data/1schema.sql
+++ data/1schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE public.Player (
     id INT PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL,
-    overall_rank INT NOT NULL,
-    position_rank VARCHAR(5) NOT NULL,
-    bye_week INT NOT NULL,
-    average_draft_position FLOAT NOT NULL,
-    can_draft BOOLEAN
+    overallRank INT NOT NULL,
+    positionRank VARCHAR(5) NOT NULL,
+    byeWeek INT NOT NULL,
+    averageDraftPosition FLOAT NOT NULL,
+    canDraft BOOLEAN
 );
 
--- ALTER TABLE public.Player OWNER TO postgres;
+ALTER TABLE public.Player OWNER TO docker;
